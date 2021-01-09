154f84a96ecdae573e3fab50ede509ab1b727d8a
zunda <47569369+zunda-pixel@users.noreply.github.com>
2021-01-10 04:29:17 +0900

    Delete __version__.py

--- __version__.py
+++
@@ -1,7 +0,0 @@
-__name__ = "sRemo"
-__version__ = "1.0.0"
-__author__ = "zunda"
-__author_email__ = "zunda-pixel@gmail.com"
-__license__ = "MIT"
-__description__ = "Python client for sRemo API"
-__url__ = "https://github.com/zunda-pixel/sRemo"

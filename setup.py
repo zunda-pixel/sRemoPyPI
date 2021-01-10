3a244487367dca4ca1b17427c4be9d45d2debe67
zunda <47569369+zunda-pixel@users.noreply.github.com>
2021-01-10 15:32:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,22 @@
 from setuptools import setup
-setup()
+from setuptools import find_packages
+
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+with open("LICENSE", "r") as f:
+    license = f.readline()
+
+setup(
+    name="sRemoAPI",
+    version="1.0.9",
+    author="zunda(@zunda_pixel)",
+    author_email="zunda.pixel@gmail.com",
+    description="Python client for sRemo API",
+    url="https://github.com/zunda-pixel/sRemo",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license=license,
+    packages=find_packages(exclude="sRemo")
+)

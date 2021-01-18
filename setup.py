8e375d0e9fd8907aed49d04fa4b67282871b93db
zunda <47569369+zunda-pixel@users.noreply.github.com>
2021-01-19 01:36:05 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
 
-with open("README.md", "r") as f:
-    description = f.read()
-
 with open("LICENSE", "r") as f:
     license = f.readline()
 
@@ -14,7 +11,6 @@ setup(
     author_email="zunda.pixel@gmail.com",
     description="Python client for sRemo(sCloud) API",
     url="https://github.com/zunda-pixel/sRemo",
-    long_description=description,
     long_description_content_type="text/markdown",
     license=license,
     packages=find_packages(exclude="sRemo")
